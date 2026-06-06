baf8532e4243f91cc7b3bfab5df85648692730d0
Nesya Aulia <nesyaauliazahrakaderisasi@gmail.com>
2026-06-06 16:59:16 +0700

    menambahkan fitur

---
+++ auryn_dinda-1801624186-drp/README.md
@@ -0,0 +1,11 @@
+# auryn_dinda-1801624186-drp
+
+Nama: Auryn Dinda Hanifa
+
+NIM: 1801624186
+
+Nama: Nesya Aulia Zahra 
+
+NIM: 1801624038
+
+
---
+++ auryn_dinda-1801624186-drp/Streakly/reward.py
@@ -0,0 +1,7 @@
+reward.py : from points import poin
+
+def tukar_reward():
+    if poin >= 50:
+        print("Reward: Silakan menonton film favorit Anda!")
+    else:
+        print("Poin belum mencukupi.")
