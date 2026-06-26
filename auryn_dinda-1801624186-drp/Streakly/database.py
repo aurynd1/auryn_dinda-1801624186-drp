220de4f23a788e227bc4e34472da24bd253324fb
aurynd1 <aurynhanifa42@gmail.com>
2026-06-26 23:10:57 +0700

    Melengkapi database dan task

--- auryn_dinda-1801624186-drp/Streakly/database.py
+++ auryn_dinda-1801624186-drp/Streakly/database.py
@@ -1,9 +1,50 @@
 import sqlite3
 
 conn = sqlite3.connect("streakly.db")
-
 cursor = conn.cursor()
 
-print("Database berhasil dibuat")
+# =========================
+# TABLE TASK
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS task(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+)
+""")
 
+# =========================
+# TABLE REWARD
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reward(
+    nama_reward TEXT,
+    deskripsi TEXT,
+    status TEXT,
+    poin_dibutuhkan INTEGER
+)
+""")
+
+# =========================
+# TABLE POINT
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS point(
+    jumlah_poin INTEGER
+)
+""")
+
+# =========================
+# TABLE STREAK
+# =========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS streak(
+    jumlah_hari INTEGER,
+    tanggal_terakhir TEXT
+)
+""")
+
+conn.commit()
 conn.close()
+
+print("Database berhasil dibuat.")
--- auryn_dinda-1801624186-drp/Streakly/task.py
+++ auryn_dinda-1801624186-drp/Streakly/task.py
@@ -1,23 +1,56 @@
 import sqlite3
+
 conn = sqlite3.connect("streakly.db")
 cursor = conn.cursor()
+
+# Membuat tabel jika belum ada
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+)
+""")
+conn.commit()
+
+# =========================
+# Fungsi Database (SQLite)
+# =========================
+
 def add_task(name):
     cursor.execute("INSERT INTO task (name) VALUES (?)", (name,))
     conn.commit()
+
+
 def get_tasks():
     cursor.execute("SELECT * FROM task")
     return cursor.fetchall()
+
+
 def update_task(id, name):
     cursor.execute("UPDATE task SET name=? WHERE id=?", (name, id))
     conn.commit()
+
+
 def delete_task(id):
     cursor.execute("DELETE FROM task WHERE id=?", (id,))
     conn.commit()
-def tambah_tugas():def 
+
+# =========================
+# Fungsi yang dipanggil main.py
+# =========================
+
+def tambah_tugas():
     nama = input("Masukkan tugas: ")
-    tugas.append(nama)
+    add_task(nama)
     print("Tugas berhasil ditambahkan.")
 
+
 def lihat_tugas():
-    for i, t in enumerate(tugas, 1):
-        print(f"{i}. {t}")
+    data = get_tasks()
+
+    if not data:
+        print("Belum ada tugas.")
+    else:
+        print("\n===== DAFTAR TUGAS =====")
+        for tugas in data:
+            print(f"{tugas[0]}. {tugas[1]}")
