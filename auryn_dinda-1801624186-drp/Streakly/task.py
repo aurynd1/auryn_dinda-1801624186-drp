9909a49b9efc70e63aa210133901601efe46183a
Nesya Aulia <nesyaauliazahrakaderisasi@gmail.com>
2026-06-26 21:47:08 +0700

    CRUD task

--- auryn_dinda-1801624186-drp/Streakly/task.py
+++ auryn_dinda-1801624186-drp/Streakly/task.py
@@ -1,4 +1,19 @@
-def tambah_tugas():
+import sqlite3
+conn = sqlite3.connect("streakly.db")
+cursor = conn.cursor()
+def add_task(name):
+    cursor.execute("INSERT INTO task (name) VALUES (?)", (name,))
+    conn.commit()
+def get_tasks():
+    cursor.execute("SELECT * FROM task")
+    return cursor.fetchall()
+def update_task(id, name):
+    cursor.execute("UPDATE task SET name=? WHERE id=?", (name, id))
+    conn.commit()
+def delete_task(id):
+    cursor.execute("DELETE FROM task WHERE id=?", (id,))
+    conn.commit()
+def tambah_tugas():def 
     nama = input("Masukkan tugas: ")
     tugas.append(nama)
     print("Tugas berhasil ditambahkan.")
