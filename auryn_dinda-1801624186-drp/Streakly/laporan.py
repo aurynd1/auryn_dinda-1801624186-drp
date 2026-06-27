68f0160740481475c52032853506d0016d092fd8
cintia nabila <cintianabila@MacBook-Air-Acil.local>
2026-06-27 19:00:41 +0700

    menambahkan menu laporan streakly

--- auryn_dinda-1801624186-drp/Streakly/laporan.py
+++ auryn_dinda-1801624186-drp/Streakly/laporan.py
@@ -52,3 +52,59 @@ def laporan_streakly():
     print(f"Task Selesai            : {task_selesai}")
     print(f"Task Belum Selesai      : {task_belum}")
     print(f"Persentase Penyelesaian : {persentase:.2f}%")
+        print("\n========== PROGRESS USER ==========")
+
+    # =========================
+    # Total Reward
+    # =========================
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM reward
+    """)
+
+    total_reward = cursor.fetchone()[0]
+
+    # =========================
+    # Reward Ditukar
+    # =========================
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM penukaran_reward
+    """)
+
+    reward_ditukar = cursor.fetchone()[0]
+
+    # =========================
+    # Total Poin
+    # =========================
+    cursor.execute("""
+        SELECT jumlah_poin
+        FROM point
+    """)
+
+    hasil = cursor.fetchone()
+
+    if hasil:
+        total_poin = hasil[0]
+    else:
+        total_poin = 0
+
+    # =========================
+    # Current Streak
+    # =========================
+    cursor.execute("""
+        SELECT jumlah_hari
+        FROM streak
+    """)
+
+    hasil = cursor.fetchone()
+
+    if hasil:
+        streak = hasil[0]
+    else:
+        streak = 0
+
+    print(f"Jumlah Reward      : {total_reward}")
+    print(f"Reward Ditukar     : {reward_ditukar}")
+    print(f"Total Poin         : {total_poin}")
+    print(f"Current Streak     : {streak} hari")
